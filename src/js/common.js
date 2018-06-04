f8f430132b64fddd0ec6d57e8c32023a396df00a
webkey <serhiy_i@ukr.net>
2018-06-04 15:54:57 +0300

    office_201800604_1550

--- src/js/common.js
+++ src/js/common.js
@@ -325,37 +325,6 @@ function slidersInit() {
 }
 
 /**
- * !Always place the footer at the bottom of the page
- * */
-function footerBottom() {
-	var $footer = $('.footer');
-
-	if ($footer.length) {
-		$('.main').after($('<div class="spacer"></div>'));
-
-		setTimeout(function () {
-			layoutFooter();
-		}, 50);
-
-		$(window).on('resizeByWidth', function () {
-			layoutFooter();
-		});
-
-		function layoutFooter() {
-			// var footerHeight = $('.footer__holder', $footer).outerHeight();
-			var footerHeight = $($footer).outerHeight();
-			$footer.css({
-				'margin-top': -footerHeight
-			});
-
-			$('.spacer').css({
-				'height': footerHeight
-			});
-		}
-	}
-}
-
-/**
  * !Testing form validation (for example). Do not use on release!
  * */
 function formSuccessExample() {
@@ -432,6 +401,5 @@ $(document).ready(function () {
 	slidersInit();
 	objectFitImages(); // object-fit-images initial
 
-	footerBottom();
 	formSuccessExample();
 });
