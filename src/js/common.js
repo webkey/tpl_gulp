71555ae8b86bc8efcc70817b97826ee670db39b6
webkey <serhiy_i@ukr.net>
2019-04-07 04:44:53 +0300

    new

--- src/js/common.js
+++ src/js/common.js
@@ -202,30 +202,45 @@ function slidersInit() {
  * !Form validation
  * */
 function formValidation() {
-  $('.user-form form').validate({
-    errorClass: "error",
-    validClass: "success",
-    errorElement: false,
-    errorPlacement: function (error, element) {
-      return true;
-    },
-    highlight: function (element, errorClass, successClass) {
-      $(element)
-          .removeClass(successClass)
-          .addClass(errorClass)
-          .closest('form').find('label[for="' + $(element).attr('id') + '"]')
-          .removeClass(successClass)
-          .addClass(errorClass);
-    },
-    unhighlight: function (element, errorClass, successClass) {
-      $(element)
-          .removeClass(errorClass)
-          .addClass(successClass)
-          .closest('form').find('label[for="' + $(element).attr('id') + '"]')
-          .removeClass(errorClass)
-          .addClass(successClass);
+  $.validator.setDefaults({
+    submitHandler: function() {
+      alert('Форма находится в тестовом режиме. Чтобы закрыть окно, нажмите ОК.');
+      return false;
     }
   });
+
+  var $form = $('.form-validate-js');
+
+  if ($form.length) {
+    var changeClasses = function (elem, remove, add) {
+      console.log('changeClasses');
+      elem
+          .removeClass(remove).addClass(add);
+      elem
+          .closest('form').find('label[for="' + elem.attr('id') + '"]')
+          .removeClass(remove).addClass(add);
+      elem
+          .closest('.input-wrap')
+          .removeClass(remove).addClass(add);
+    };
+
+    $.each($form, function (index, element) {
+      $(element).validate({
+        errorClass: "error",
+        validClass: "success",
+        errorElement: false,
+        errorPlacement: function (error, element) {
+          return true;
+        },
+        highlight: function (element, errorClass, successClass) {
+          changeClasses($(element), successClass, errorClass);
+        },
+        unhighlight: function (element, errorClass, successClass) {
+          changeClasses($(element), errorClass, successClass);
+        }
+      });
+    });
+  }
 }
 
 /**
